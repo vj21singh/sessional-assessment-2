541a4608fd200e7173b204f93f407f517d5036f8
Vijay <vijay1772.be20@chitkara.edu.in>
2022-08-11 15:46:22 +0530

    Added Site title and Blog carousel

--- js/main.js
+++ js/main.js
@@ -22,5 +22,15 @@ $(document).ready(function () {
     $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
     })
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
 
 });
